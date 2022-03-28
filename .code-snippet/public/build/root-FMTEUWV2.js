becf0cb62d7197d742167022a12823034a811cf3
Sona Juhasova <soj@web2media.dk>
2022-03-28 21:57:14 +0200

    edit

---
+++ .code-snippet/public/build/root-FMTEUWV2.js
@@ -0,0 +1,35 @@
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration
+} from "/build/_shared/chunk-XBNIDT4G.js";
+import {
+  React,
+  init_react
+} from "/build/_shared/chunk-P2FTGPOX.js";
+
+// browser-route-module:/Users/sonajuhasova/code-snippet-remix-app/code-snippet/app/root.jsx?browser
+init_react();
+
+// app/root.jsx
+init_react();
+function meta() {
+  return {
+    charset: "utf-8",
+    title: "New Remix App",
+    viewport: "width=device-width,initial-scale=1"
+  };
+}
+function App() {
+  return /* @__PURE__ */ React.createElement("html", {
+    lang: "en"
+  }, /* @__PURE__ */ React.createElement("head", null, /* @__PURE__ */ React.createElement(Meta, null), /* @__PURE__ */ React.createElement(Links, null)), /* @__PURE__ */ React.createElement("body", null, /* @__PURE__ */ React.createElement(Outlet, null), /* @__PURE__ */ React.createElement(ScrollRestoration, null), /* @__PURE__ */ React.createElement(Scripts, null), /* @__PURE__ */ React.createElement(LiveReload, null)));
+}
+export {
+  App as default,
+  meta
+};
+//# sourceMappingURL=/build/root-FMTEUWV2.js.map
